7276b8ea1464c45f2e6f2fc3f689872c6a1ae046
Aryan Sapra <aryansaprajiit@gmail.com>
2025-08-18 16:03:50 +0530

    client implemented

--- src/main/java/org/lowleveldesign/behaviouralPatterns/observerPattern/withObserverPattern/WeatherStation.java
+++ src/main/java/org/lowleveldesign/behaviouralPatterns/observerPattern/withObserverPattern/WeatherStation.java
@@ -30,21 +30,4 @@ public class WeatherStation implements SubjectI{
             observer.update(temperature);
         }
     }
-    public static void main(String[] args) {
-        // Example usage of the WeatherStation class.
-        WeatherStation weatherStation = new WeatherStation();
-
-        DevicesI mobile = new Mobile();
-        DevicesI appleWatch = new AppleWatch();
-
-        weatherStation.registerObserver(mobile);
-        weatherStation.registerObserver(appleWatch);
-
-        // Simulate a temperature change.
-        weatherStation.notifyObservers(25);
-
-        // Unregister the mobile device and simulate another temperature change.
-        weatherStation.unregisterObserver(mobile);
-        weatherStation.notifyObservers(30);
-    }
 }
